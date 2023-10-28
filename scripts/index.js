24872e1a7a57477d7886e5106361f8bbdf2a8511
LeilaC1 <leilaclesca13@gmail.com>
2023-10-28 17:18:10 -0400

    completed exercise

--- scripts/index.js
+++ scripts/index.js
@@ -14,25 +14,63 @@ function onButtonClicked() {
     // input
     const inputPickup = document.getElementById("inputPickup");
     const inputDays = document.getElementById("inputDays");
-
-
+  let days = Number(inputDays.value);
+    let carRentalResults = (29.99 * days)
+    let optionsADay = 0;
+    let under25 = 0;
+  
+   
     // checks
-    const tollCheckChecked = document.getElementById("tollCheck").checked;
-    const gpsCheckChecked = document.getElementById("gpsCheck").checked;
-    const roadsideCheckChecked = document.getElementById("roadsideCheck").checked;
+    let tollChecked = document.getElementById("tollCheck").checked;
+    let gpsChecked = document.getElementById("gpsCheck").checked;
+    let roadsideChecked = document.getElementById("roadsideCheck").checked;
 
 
     // radio
-    const noRadioChecked = document.getElementById("noRadio").checked;
-    const yesRadioChecked = document.getElementById("yesRadio").checked;
+    let yesRadioChecked = document.getElementById("yesRadio").checked;
+
+    
+    //let
+  
+
+// ifelse
+
+if (tollChecked) {
+    optionsADay += 3.95;
+    
+}  if (gpsChecked) {
+    optionsADay += 2.95;
+    
+}  if (roadsideChecked) {
+    optionsADay += 2.95;
+    
+} 
 
 
-    // output
-    const carRental = document.getElementById("carRental").value;
-    const options = document.getElementById("options").value;
-    const surcharge = document.getElementById("surcharge").value;
+if (yesRadioChecked){
+    under25 = 0.3;
+
+} 
+
+// lets for ifs
+ let optionsResults = optionsADay * days;
+let surchargeResults = carRentalResults + optionsResults * under25;
+let totalDueResults = carRentalResults + optionsResults + surchargeResults;
 
 
+
+  // output const
+  const carRental = document.getElementById("carRental");
+  const options = document.getElementById("options");
+  const surcharge = document.getElementById("surcharge");
+  const totalDue = document.getElementById("totalDue");
+
+
+// output innerhtml
+carRental.innerHTML = carRentalResults.toFixed(2) ;
+options.innerHTML = optionsResults.toFixed(2);
+surcharge.innerHTML = surchargeResults.toFixed(2);
+totalDue.innerHTML = totalDueResults.toFixed(2);
 
 
 }
